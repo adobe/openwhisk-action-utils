3129aae2f9b11993339f73a8e8eb70a10c34f0e7
Tobias Bocanegra <tripod@adobe.com>
2019-11-25 16:41:14 +0900

    chore(middleware): remove code duplicate

--- src/middleware.js
+++ src/middleware.js
@@ -10,8 +10,6 @@
  * governing permissions and limitations under the License.
  */
 const crypto = require('crypto');
-const bunyan = require('bunyan');
-const { BunyanStreamInterface, eraseBunyanDefaultFields, rootLogger } = require('@adobe/helix-log');
 
 /**
  * Helper functions for expressified actions.
@@ -164,52 +162,9 @@ function asyncHandler(fn, params) {
   return (req, res, next) => (Promise.resolve(fn(params, req, res, next)).catch(next));
 }
 
-/**
-* Bunyan serializers
-* @private
-*/
-const serializers = {
-  res: (res) => {
-    if (!res || !res.statusCode) {
-      return res;
-    }
-    return {
-      statusCode: res.statusCode,
-      duration: res.duration,
-      headers: res.getHeaders(),
-    };
-  },
-  req: bunyan.stdSerializers.req,
-  err: bunyan.stdSerializers.err,
-};
-
-/**
- * Sets up a bunyan logger suitable to use with an openwhisk action. The bunyan logger will
- * stream to the given helix logger.
- *
- * @param {Logger} [logger=rootLogger] - a helix multi logger. defaults to the helix `rootLogger`.
- * @return {BunyanLogger} A bunyan logger
- */
-function createBunyanLogger(logger = rootLogger) {
-  return bunyan.createLogger({
-    name: 'action',
-    serializers,
-    streams: [{
-      name: 'BunyanStreamInterface',
-      level: 'trace',
-      type: 'raw',
-      stream: new BunyanStreamInterface({
-        logger,
-        filter: eraseBunyanDefaultFields,
-      }),
-    }],
-  });
-}
-
 module.exports = {
   errorHandler,
   cacheControl,
   logRequest,
   asyncHandler,
-  createBunyanLogger,
 };
